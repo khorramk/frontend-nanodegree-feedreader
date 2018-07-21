0818f93c44355bb9615996cfd9d9b9810c4425ff
khorramk <khorramk.kbsk@gmail.com>
2018-07-21 23:52:46 +0100

    updated the all files

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,22 +1,23 @@
 $(function () {
 
     describe('RSS Feeds', function () {
-
+        //make sure all RSS feeds are defined
+        //and it has objects
         it('are defined', function () {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).toBeGreaterThan(0);
         });
 
 
-
+        //looping through all the feeds and making sure all the links are defined and present
         it('loops through allfeeds', function () {
             allFeeds.forEach(function (obj) {
                 expect(obj.url).toBeDefined();
                 expect(obj.url.length).toBeGreaterThan(0);
             })
         })
-
-
+        
+        //looping through the feeds and make sure names are present.
         it('name defined in feeds', function () {
             allFeeds.forEach(function (obj) {
                 expect(obj.name).toBeDefined();
@@ -27,9 +28,7 @@ $(function () {
 
 
     describe('The menu', function () {
-
-
-
+        //cheking whether the menu is hidden by defualt or not
         const menu = document.querySelector('.menu-hidden');
         const clickableMenu = document.querySelector('.menu-icon-link');
         it('menu hidden', function () {
@@ -37,6 +36,7 @@ $(function () {
             expect(menu.classList.contains('menu-hidden')).toBe(true);
         })
 
+        //cheking the visibility of menu upon user clicking the menu button
         it('visibility of menu', function () {
 
             var simulateClick = function (elem) {
@@ -53,30 +53,27 @@ $(function () {
 
             simulateClick(clickableMenu);
 
-
-            expect(menu.classList.contains('menu-hidden')).toBe(false);
-
+            expect($('body').hasClass('menu-hidden').toBe(false);
 
             simulateClick(clickableMenu);
 
-
-            expect(menu.classList.contains('menu-hidden')).toBe(true);
-
-
+            expect($('body').hasClass('menu-hidden').toBe(true);
 
 
         });
     });
+
     describe('Initial Entries', function () {
 
 
-
-
-        beforeEach(function (done) {
+        //cheking whether the loading the feed is complete or not
+        beforeEach(function(done) {
             loadFeed(0, function () {
                 done();
             })
         });
+
+        //once feed is loaded check whether the entries exist or not and have values
         it('load Feed', function () {
             const entries = document.querySelectorAll('.feed .entry');
             expect(entries.length).toBeGreaterThan(0);
@@ -86,6 +83,7 @@ $(function () {
 
     describe('New Feed Selection', function () {
 
+        //cheking whether the feeds are in correct places or not
         var firstfeed, secondFeed;
         beforeEach(function (done) {
             loadFeed(1, function () {
@@ -99,6 +97,7 @@ $(function () {
             })
         });
 
+        //making sure all the feeds are not same
         it('changing content', function () {
             expect(firstfeed).not.toBe(secondFeed);
         });
--- runningTheApplication.md
+++
@@ -1,6 +0,0 @@
-# running the test
-
-1. open the index.html with your browser
-2. see whether all test passed or failed
-3. if failed check the [feedreader.js](jasmine/spec/feedreader.js) file.
-4. and see the code wrtie the test. again.
